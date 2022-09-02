19ccb52a3937c8f6b503ad7fa6b7e73057ba5350
Marten Koopmans <marten.koopmans@ordina.nl>
2022-09-02 23:51:47 +0200

    Add plot settings to sidebar

--- pages/plot_page.py
+++ pages/plot_page.py
@@ -1,3 +1,4 @@
+from turtle import width
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,17 +6,17 @@ import numpy as np
 import seaborn as sns
 
 
-
 data_var = pd.read_csv('SimSS/Var.dat', delim_whitespace=True)
 data_jv = pd.read_csv('SimSS/JV.dat', delim_whitespace=True)
 
-options = st.multiselect(
-     'Which parameters would you like to plot on the y-axis?',
-     list(data_var.columns),
-     ['V'])
+with st.sidebar:
+    options = st.multiselect(
+        'Which parameters would you like to plot on the y-axis?',
+        list(data_var.columns),
+        ['V'])
 
-scale_y = ['linear','log']
-choice_y_scale = st.selectbox('y-scale', scale_y)
+    scale_y = ['linear','log']
+    choice_y_scale = st.selectbox('y-scale', scale_y)
 
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
@@ -24,6 +25,7 @@ ax2.set_yscale(choice_y_scale)
 sns.scatterplot(data=data_jv, x='Vext', y='Jext', ax=ax1)
 for y_var in options:
     sns.scatterplot(data=data_var, x='x', y=y_var, ax=ax2, label=y_var)
+
 
 st.pyplot(fig1, format='png')
 st.pyplot(fig2, format='png')
