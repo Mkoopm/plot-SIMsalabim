49cd56d54d1f81fe2be4263db52bd82cb4f6e58b
Marten Koopmans <marten.koopmans@ordina.nl>
2022-09-05 14:59:00 +0200

    Make device parameters persistent

--- pages/dev_par_page.py
+++ pages/dev_par_page.py
@@ -6,7 +6,7 @@ from subprocess import run
 
 SimSS_path = 'SIMsalabim/SimSS/'
 dev_par_init = []
-with open(SimSS_path+'device_parameters_backup.txt') as fp:
+with open(SimSS_path+'device_parameters.txt') as fp:
     for line in fp:
         dev_par_init.append(line)
 
@@ -20,9 +20,12 @@ dev_par_text = st.text_area(
 with open(SimSS_path+'device_parameters.txt', 'w') as fp:
     fp.write(dev_par_text)
 
-with st.spinner('SIMulating...'):
-    result = run('./simss', cwd=SimSS_path)
+def run_simss():
+    with st.spinner('SIMulating...'):
+        result = run('./simss', cwd=SimSS_path)
 
-if result.returncode != 0:
-    st.error('SIMsalabim raised an error')
+    if result.returncode != 0:
+        st.error('SIMsalabim raised an error')
+
+st.button('Run SimSS', on_click=run_simss)
 #st.success('Done!')
